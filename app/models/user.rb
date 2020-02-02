f501adea09f3e01e357e0979b83be688edebaef4
nuigurumi03 <nuigurumi03@gmail.com>
2020-02-02 19:24:20 +0900

    profileテーブル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  
+  has_one :profile, dependent: :destroy
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  PASSWORD_VALIDATION = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{7,128}+\z/i
+  
+  validates :nickname,              presence: true, length: {maximum: 20}
+  validates :email,                 presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
+  validates :password,              presence: true, length: {minimum: 7, maximum: 128},    format: { with: PASSWORD_VALIDATION }
+  validates :password_confirmation, presence: true, length: {minimum: 7, maximum: 128}
 end
